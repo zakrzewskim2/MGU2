435324549a4efce407b8655134c65a8970d02e3f
Marcin Zakrzewski <zakrzewskim2@student.mini.pw.edu.pl>
2020-04-27 12:07:26 +0200

    add 20 and 21 arch
    Signed-off-by: Marcin Zakrzewski <zakrzewskim2@student.mini.pw.edu.pl>

---
+++ 20_adam_opt_final_arch_.py
@@ -0,0 +1,110 @@
+import keras
+from keras.datasets import cifar10
+from keras.models import Sequential
+from keras.utils import np_utils
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Dense, Activation, Flatten, Dropout, BatchNormalization
+from keras.layers import Conv2D, MaxPooling2D
+from keras import regularizers
+from keras.callbacks import LearningRateScheduler
+import numpy as np
+
+def lr_schedule(epoch):
+    lrate = 0.001
+    if epoch > 60:
+        lrate = 0.0005
+    if epoch > 90:
+        lrate = 0.0003
+    if epoch > 100:
+        lrate = 0.00015
+    if epoch > 125:
+      lrate = 0.00005
+    return lrate
+ 
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+ 
+#z-score
+mean = np.mean(x_train,axis=(0,1,2,3))
+std = np.std(x_train,axis=(0,1,2,3))
+x_train = (x_train-mean)/(std+1e-7)
+x_test = (x_test-mean)/(std+1e-7)
+ 
+num_classes = 10
+y_train = np_utils.to_categorical(y_train,num_classes)
+y_test = np_utils.to_categorical(y_test,num_classes)
+
+#%%
+
+weight_decay = 1e-4
+model = Sequential()
+model.add(Conv2D(64, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay), input_shape=x_train.shape[1:]))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(64, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.3))
+ 
+model.add(Conv2D(128, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(128, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.4))
+ 
+model.add(Conv2D(256, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(256, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.5))
+ 
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.summary()
+ 
+#data augmentation
+datagen = ImageDataGenerator(
+    rotation_range=15,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    horizontal_flip=True,
+    )
+datagen.fit(x_train)
+ 
+#training
+batch_size = 64
+ 
+opt_rms = keras.optimizers.adam(lr=0.001)
+model.compile(loss='categorical_crossentropy', optimizer=opt_rms, metrics=['accuracy'])
+history = model.fit_generator(datagen.flow(x_train, y_train, batch_size=batch_size),\
+                    steps_per_epoch=x_train.shape[0] // batch_size,epochs=150,\
+                    verbose=1,validation_data=(x_test,y_test),callbacks=[LearningRateScheduler(lr_schedule)])
+#save to disk
+arch_num = "20-adam"
+
+from google.colab import drive
+drive.mount('/content/gdrive', force_remount=True)
+root_dir = "/content/gdrive/My Drive/Colab Notebooks/"
+import pandas as pd
+hist_df = pd.DataFrame(history.history)
+
+model_json = model.to_json()
+with open(f'{root_dir}{arch_num}-model.json', 'w') as json_file:
+    json_file.write(model_json)
+with open(f'{root_dir}{arch_num}-history.json', 'w') as f:
+    hist_df.to_json(f)
+model.save_weights(f'{root_dir}{arch_num}-model.h5') 
+ 
+#testing
+scores = model.evaluate(x_test, y_test, batch_size=128, verbose=1)
+print('\nTest result: %.3f loss: %.3f' % (scores[1]*100,scores[0]))
